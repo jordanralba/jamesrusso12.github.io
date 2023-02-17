2132ceac66299142369e3c7bd059fb255d40efc1
Jordan Alba Reed <124318039+jordanralba@users.noreply.github.com>
2023-02-17 15:32:23 -0700

    Last Verification
    Reuploaded the folder with the same structure in order to verify all edits were processed and comitted

--- bootstrap/js/jquery.fullPage.js
+++ bootstrap/js/jquery.fullPage.js
@@ -94,8 +94,8 @@
             //scrolling
             css3: true,
             scrollingSpeed: 700,
-            autoScrolling: true,
-            fitToSection: true,
+            autoScrolling: false,
+            fitToSection: false,
             easing: 'easeInOutCubic',
             easingcss3: 'ease',
             loopBottom: false,
@@ -120,7 +120,7 @@
             sectionsColor : [],
             paddingTop: 0,
             paddingBottom: 0,
-            fixedElements: null,
+            fixedElements: '.notAutoScroll',
             responsive: 0,
 
             //Custom selectors
